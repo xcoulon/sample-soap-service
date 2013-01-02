a33903321a6001d19b6834c965dc11a06d6c50ba
Xavier Coulon <xcoulon@redhat.com>
2013-01-02 18:08:11 +0100

    Commit from JBoss Tools

--- src/main/java/sample/HelloWorldImpl.java
+++ src/main/java/sample/HelloWorldImpl.java
@@ -1,9 +1,11 @@
 package sample;
  
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
  
 //Service Implementation
 @WebService(endpointInterface = "sample.HelloWorld", serviceName="HelloWorld" )
+@SOAPBinding(parameterStyle = SOAPBinding.ParameterStyle.BARE)
 public class HelloWorldImpl implements HelloWorld{
  
 	@Override
